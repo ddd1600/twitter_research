c4da05cd3a7995477cd2bccf2d32e9768c0024d3
David Douglas <daviddouglas1949@gmail.com>
2012-06-27 11:41:18 -0600

    simple twitter installed and working

--- app/controllers/searches_controller.rb
+++ app/controllers/searches_controller.rb
@@ -1,2 +1,13 @@
 class SearchesController < ApplicationController
+  
+  def index
+     @message = "What up!"
+  end
+  
+  def show
+    instance = SimpleTwitter::Search.new
+    @results = instance.search(params[:q])
+  
+  end
+  
 end
--- config/routes.rb
+++ config/routes.rb
@@ -52,10 +52,11 @@ TwitterResearch::Application.routes.draw do
   # You can have the root of your site routed with "root"
   # just remember to delete public/index.html.
   root :to => 'searches#index'
+  resources :searches
 
   # See how all your routes lay out with "rake routes"
 
   # This is a legacy wild controller route that's not recommended for RESTful applications.
   # Note: This route will make all actions in every controller accessible via GET requests.
-  # match ':controller(/:action(/:id))(.:format)'
+  #match ':controller(/:action(/:id))(.:format)'
 end
